f45872c84cb9230fa8108b33b88a0f3eb4ec1c1f
tugi <blueqwinn@gmail.com>
2023-02-23 17:27:41 +0300

    add files

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
-  
+    def change
+        create_table :Actor do |t|
+          t.string :name
+          t.timestamps
+        end
+    
+        create_table :Actor do |t|
+          t.belongs_to :actor
+          t.datetime :publisher
+          t.timestamps
+          t.belongs_to :actor, foreign_key: true
+        end
+      end
+    end
+    class Series < ApplicationRecord
+        has_one :acts
+      end
+
+
+
+
 end
